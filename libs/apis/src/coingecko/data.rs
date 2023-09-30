b82c775beb5d0e88f73f9a1a330c3fb2ed57e338
0xMimir <mimirz@proton.me>
2023-09-30 23:47:09 +0200

    added get info

--- libs/apis/src/coingecko/data.rs
+++ libs/apis/src/coingecko/data.rs
@@ -15,3 +15,45 @@ impl CryptoInfo {
         self.github.is_none() && self.gitlab.is_none() && self.description.is_none()
     }
 }
+
+#[derive(Deserialize)]
+pub(crate) struct CryptoInfoResponse {
+    pub description: Localization,
+    pub links: Links,
+}
+
+#[derive(Deserialize)]
+pub(crate) struct Localization {
+    pub en: Option<String>,
+}
+
+#[derive(Deserialize)]
+pub(crate) struct Links {
+    pub repos_url: ReposUrl,
+}
+
+#[derive(Deserialize)]
+pub(crate) struct ReposUrl {
+    pub github: Vec<String>,
+}
+
+impl From<CryptoInfoResponse> for CryptoInfo {
+    fn from(value: CryptoInfoResponse) -> Self {
+        let description = value.description.en;
+        let github = value.links.repos_url.extract_github();
+
+        Self {
+            github,
+            gitlab: None,
+            description,
+        }
+    }
+}
+
+impl ReposUrl {
+    pub fn extract_github(self) -> Option<String> {
+        let url = self.github.into_iter().next()?;
+        let url = url.replace("https://github.com/", "");
+        Some(url.split_once('/')?.0.to_owned())
+    }
+}
